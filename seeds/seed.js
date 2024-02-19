c6df57342f90f4848a75a92f4d6d3644758f29d8
wileland <wileland7@gmail.com>
2024-02-18 20:56:11 -0600

    Update login script and user data***

--- seeds/seed.js
+++ seeds/seed.js
@@ -4,12 +4,15 @@ const { User } = require('../models/User.js');
 const userData = require('./userData.json');
 
 const seedDatabase = async () => {
-  await sequelize.sync({ force: true });
+  await sequelize.sync({ force: true }).catch(err => console.error('Error syncing database', err));
 
+  console.log(sequelize);
+  console.log(User);
   await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
+
 
   process.exit(0);
 };
